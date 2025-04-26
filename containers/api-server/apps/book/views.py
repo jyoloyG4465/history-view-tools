90822de1579f5d83af2c608e04363fcb50499aa5
jyoroyG <sriki1552@gmail.com>
2025-04-26 10:34:48 +0900

    create demo-api

--- containers/api-server/apps/book/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-@api_view(['GET'])
-def sample(request):
-    if request.method == 'GET':
-        # モデルインスタンスを直接シリアライズし、JSONに変換
-        profiles_data = [{'id': 1} for profile in [1,2]]
-        return Response(profiles_data)
--- containers/api-server/common/models/book/models.py
+++ containers/api-server/common/models/book/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Book(models.Model):
+    book_id = models.AutoField(primary_key=True) 
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
-    aaaaaa = models.CharField(max_length=100, null=True)
 
     class Meta:
         app_label = 'apps'
