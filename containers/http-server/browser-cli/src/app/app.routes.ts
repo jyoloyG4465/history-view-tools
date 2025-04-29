d76b74c74e54a69a53611016ad583eaea30948c8
jyoroyG <sriki1552@gmail.com>
2025-04-29 18:13:28 +0900

    add router-outret and create analysis-component

--- containers/http-server/browser-cli/src/app/app.routes.ts
+++ containers/http-server/browser-cli/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DatasetComponent } from './pages/dataset/dataset.component';
+import { AnalysisComponent } from './pages/analysis/analysis.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // これがリダイレクト
+  { path: 'home', component: HomeComponent },
+  { path: 'dataset', component: DatasetComponent },  
+  { path: 'analysis', component: AnalysisComponent } 
+];
---
+++ containers/http-server/browser-cli/src/app/pages/dataset/dataset.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dataset',
+  imports: [],
+  templateUrl: './dataset.component.html',
+  styleUrl: './dataset.component.scss'
+})
+export class DatasetComponent {
+
+}
