82e4694785f02da1729e7b7740a9a0bce5f60334
jyoroyG <sriki1552@gmail.com>
2025-04-29 22:29:21 +0900

    adjust header and navigationbar

--- containers/http-server/browser-cli/src/app/app.component.ts
+++ containers/http-server/browser-cli/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ containers/http-server/browser-cli/src/app/layout/navigation/navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [MatListModule, RouterModule],
+  templateUrl: './navigation.component.html'
+})
+export class NavigationComponent {}
