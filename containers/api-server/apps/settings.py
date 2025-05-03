8c22f44fe97490bacd01dc39dc1cd42ee3c70ddd
jyoroyG <sriki1552@gmail.com>
2025-05-03 11:46:57 +0900

    enable communication between the frontend and the backend."

--- containers/api-server/apps/settings.py
+++ containers/api-server/apps/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
 ]
 
 ROOT_URLCONF = 'apps.urls'
