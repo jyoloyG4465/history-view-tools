37ff5fefcbb0ad9acf68ccedcdd9c97402494fe7
jyoroyG <sriki1552@gmail.com>
2025-05-12 21:47:22 +0900

    Rough layout adjustment of analysis screen

--- containers/http-server/browser-cli/src/app/pages/analysis/analysis.component.ts
+++ containers/http-server/browser-cli/src/app/pages/analysis/analysis.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LoadingSpinnerComponent } from '@app/shared/loading-spinner/loading-spinner.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-analysis',
-  imports: [],
+  imports: [TranslateModule, LoadingSpinnerComponent],
   templateUrl: './analysis.component.html',
   styleUrl: './analysis.component.scss',
 })
