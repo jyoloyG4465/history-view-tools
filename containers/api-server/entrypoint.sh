9e2842c770d65d9a4ca58edbe280a3c4a25643d9
jyoroyG <sriki1552@gmail.com>
2025-05-24 23:58:00 +0900

    adjust

--- containers/api-server/entrypoint.sh
+++ containers/api-server/entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
 echo "Waiting for PostgreSQL..."
-
 while ! nc -z databases 5432; do
   sleep 0.5
 done
-
 echo "PostgreSQL is up - applying migrations"
 python manage.py migrate
-
 echo "Starting server"
 exec python -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000
