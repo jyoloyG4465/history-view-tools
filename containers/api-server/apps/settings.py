5275c59234a1ec0055d40c5f07f91570b75a1727
jyoroyG <sriki1552@gmail.com>
2025-05-24 23:35:14 +0900

    release action

--- containers/api-server/apps/settings.py
+++ containers/api-server/apps/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
-    # "http://localhost:8080",
+    "http://localhost:8080",
 ]
 
 CORS_ALLOW_METHODS = [
--- host_script.sh
+++ host_script.sh
@@ -6,9 +6,11 @@ echo "コンテナが立ち上がりました！ホスト側の操作を実行�
 DIR="./postgres_data"
 OWNER=$(stat -c '%U' "$DIR")
 echo "$OWNER"
+CURRENT_USER=$(whoami)
+CURRENT_GROUP=$(id -gn)
 
 if [ "$OWNER" = "UNKNOWN" ]; then
-  sudo chown -R r3-yamada:r3-yamada "$DIR"
+  sudo chown -R $CURRENT_USER:$CURRENT_GROUP "$DIR"
 else
   echo "所有者は $OWNER なので、chown は不要です。"
 fi
