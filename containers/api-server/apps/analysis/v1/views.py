265f1ee6c1e2f06c4b88559a2c8ec6f2e9ba3dbc
jyoroyG <sriki1552@gmail.com>
2025-07-07 21:50:25 +0900

    update github actions

--- containers/api-server/apps/analysis/v1/views.py
+++ containers/api-server/apps/analysis/v1/views.py
@@ -1,16 +1,16 @@
+from apps.analysis.v1 import services
+from common.constants import Constants
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
-
-from apps.analysis.v1 import services
-from common.constants import Constants
 
 
 @api_view(["GET"])
 def get_channel_list(request: Request):
     dataset_id = request.query_params.get(Constants.KEY_DATASET_ID)
     response = services.get_channel_list(dataset_id)
+
     return Response(response, status=status.HTTP_200_OK)
 
 
