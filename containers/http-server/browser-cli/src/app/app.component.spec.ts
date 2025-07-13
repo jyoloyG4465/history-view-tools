9d7f2537537e3b339eab5314e65749411320700e
jyoroyG <sriki1552@gmail.com>
2025-07-13 13:46:08 +0900

    update github actions

--- containers/http-server/browser-cli/src/app/app.component.spec.ts
+++ containers/http-server/browser-cli/src/app/app.component.spec.ts
@@ -36,13 +36,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('browser-cli');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, browser-cli'
-    );
-  });
 });
