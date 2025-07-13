ddb72dbdaa85ab0660a0a5ee882b7ebe71f185be
jyoroyG <sriki1552@gmail.com>
2025-07-13 14:49:02 +0900

    update github actions unittest

--- containers/http-server/browser-cli/src/app/pages/analysis/analysis-graph/analysis-graph.component.spec.ts
+++ containers/http-server/browser-cli/src/app/pages/analysis/analysis-graph/analysis-graph.component.spec.ts
@@ -4,6 +4,7 @@ import { AnalysisGraphViewComponent } from './analysis-graph-view/analysis-graph
 import { AnalysisGraphSettingsComponent } from './analysis-graph-settings/analysis-graph-settings.component';
 import { CommonModule } from '@angular/common';
 import { NgxsModule } from '@ngxs/store';
+import { NGX_ECHARTS_CONFIG } from 'ngx-echarts';
 
 describe('AnalysisGraphComponent', () => {
   let component: AnalysisGraphComponent;
@@ -18,6 +19,7 @@ describe('AnalysisGraphComponent', () => {
         AnalysisGraphComponent, // Standalone Componentの場合
         NgxsModule.forRoot([]), // これを追加
       ],
+      providers: [{ provide: NGX_ECHARTS_CONFIG, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AnalysisGraphComponent);
